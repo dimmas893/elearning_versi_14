f431d4689a88ca74677d7085aa08997f771babaa
dimmas <anandadimmas1204@gmail.com>
2022-09-06 17:07:15 +0700

    first

---
+++ app/Models/Ulangan.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ulangan extends Model
+{
+    use HasFactory;
+    protected $table = 'ulangan';
+    protected $fillable = [
+        'jadwal_id',
+        'judul',
+        'type',
+        'file_or_link',
+        'description',
+        'pengumpulan',
+        'tanggal',
+        'pertemuan'
+    ];
+
+    public function jadwal()
+    {
+        return $this->belongsTo(Jadwal::class);
+    }
+}
